34483d7224f97afa68a8c44820b4fc033e80dac2
Vitaly09091967 <120189317+Vitaly09091967@users.noreply.github.com>
2024-04-06 22:12:50 +0300

    Homework4

---
+++ mysite/myapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from .forms import ProductForm
+
+def create_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+    return render(request, 'create_product.html', {'form': form})
+
+
+# Create your views here.
